59f128a4b532792f09df16a537a88ee816de6ae8
Ilua Kisialiou <iluxa91@inbox.ru>
2022-08-29 14:09:13 +0300

    Updates

---
+++ precache-manifest.00d1d450c12d91c0d0cbdfe2af148d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecc814445969f0882c6c8391d7564f2",
+    "url": "/todolist-redux-toolkit/index.html"
+  },
+  {
+    "revision": "596eb860379951760081",
+    "url": "/todolist-redux-toolkit/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todolist-redux-toolkit/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-redux-toolkit/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596eb860379951760081",
+    "url": "/todolist-redux-toolkit/static/js/main.0292386f.chunk.js"
+  },
+  {
+    "revision": "3fc49d727be666dae7a0",
+    "url": "/todolist-redux-toolkit/static/js/runtime-main.175c7478.js"
+  }
+]);
